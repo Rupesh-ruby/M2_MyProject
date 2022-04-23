e66ccb0e7dea74f8c54a1e354f58c56ddea46409
Rupesh <82668307+Rupesh-ruby@users.noreply.github.com>
2022-04-23 13:40:28 +0530

    Update Project.c

--- 3_Implemenation/Project.c
+++ 3_Implemenation/Project.c
@@ -1,100 +1,18 @@
-
-
-/**
-
- * @file Project.c
-
- * @brief Project to detect the door is opened or not with the indication of LED glow and Buzzer sound that are connecte
-
- */
-
-#include "project_config.h"
-
-#include "user_utils.h"
-
-#include "detector.h"
-
-
-/**
-
-
-
-void peripheral_init(void)
-
-{
-
-	/* Configure LED Pin */	DDRC=DDRC|0b01000000;
-
-  
-
-
-
-  DDRD = 0xff;  
-
-  
-
-  /* Configure Door_sensor Pin */
-
-  DDRB=DDRB&0b11111101;
-
-}
-
-void change_led_state(uint8_t state)
-
-{
-
-	LED_PORT = (state << LED_PIN);
-
-  BUZZER_PORT = (state << BUZZER_PIN);
-
-}
-
-/**
-
- * @brief Main function where the code execution starts
-
- * 
-
- * @return int Return 0 if the program completes successfully
-
- * @note PORTC6 is in sink config. i.e when PORTB1 is in ON condition, the LED will turn ON
-
- * @note PORTC6 is in sink config. i.e when PORTB1 is in OFF condition, the LED will turn OFF
-
- * @note PORTD1 is in sink config. i.e when PORTB1 is in ON condition, the Buzzer will turn ON and produce the beep sound
-
- * @note PORTD1 is in sink config. i.e when PORTB1 is in OFF condition, the Buzzer will turn OFF  
-
- */
-
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
 int main(void)
-
 {
-
-	/* Initialize Peripherals */
-
-	peripheral_init();
-
-	  while(1)
-
-  {
-
-if(PINB & 0b00000010)
-
-{
-
-  PORTC=PORTC|0b01000000;// Turn ON the LED connected to PORTC
-
-  PORTD = 0xff;        // Turn ON the Buzzer conneted to PORTD
-
-}
-
-else{
-
-    PORTC=PORTC&0b10111111;// Turn OFF the LED connected to PORTC
-
-    PORTD = 0x00;        // Turn OFF the Buzzer connected to PORTD
-
-}
-
-  }
+	DDRB=0xFF;
+	unsigned char z;
+	while(1)
+	{
+		
+		for(z=0;z<255;z++)
+		{
+			PORTB=z;
+			_delay_ms(1000);
+		}
+	}
+	return 0;
+	}
